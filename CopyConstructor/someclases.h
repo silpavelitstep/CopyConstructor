97779a90e48bac39b28d3e90ccd4ca91064c82a5
silpavel@outlook.com <silpavel@outlook.com>
2019-05-06 18:18:20 +0300

    some classes with constructor without implementation

--- CopyConstructor/someclases.h
+++ CopyConstructor/someclases.h
@@ -1,1 +1,45 @@
 #pragma once
+#ifndef SOMECLASES_H
+#define SOMECLASES_H
+
+//Base class
+class A {
+public:
+	A();
+	~A();
+};
+//clases B, C, D extends A
+class B :public A{
+public:
+	B();
+	~B();
+};
+class C : public A {
+public:
+	C();
+	~C();
+};
+class D : public A {
+public:
+	D();
+	~D();
+};
+//class E extends B, class F extends C and D
+class E : public B {
+public:
+	E();
+	~E();
+};
+class F :public C, public D {
+public:
+	F();
+	~F();
+};
+//class G extends E and F
+class G : public E, public F {
+public:
+	G();
+	~G();
+};
+
+#endif SOMECLASES_H
