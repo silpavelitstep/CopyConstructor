bc80eaa7b98ba9ea586a4578891f52e949e7a131
silpavel@outlook.com <silpavel@outlook.com>
2019-05-06 18:40:37 +0300

    test ierarhy + #

--- CopyConstructor/main.cpp
+++ CopyConstructor/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "someclasses.h"
+//#define TI "ON"
 using namespace std;
 int main() {
+#ifdef TI
 	//test how much constructor and destructor executed in stack
 	{
 		G g;
@@ -9,6 +11,7 @@ int main() {
 	//test how much constructor and destructor executed in heap
 	G *pg = new G;
 	delete pg;
+#endif
 
 	system("pause");
 	return 0;
