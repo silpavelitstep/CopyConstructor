3977bd8ff3a31645828620f39fa78fb9d8b3ec0a
silpavel@outlook.com <silpavel@outlook.com>
2019-05-06 18:24:35 +0300

    Constructor/destructor added.

---
+++ CopyConstructor/someclases.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "someclases.h"
+using namespace std;
+A::A() {cout << "cons A()\n";}
+A::~A() { cout << "des ~A()\n";}
+B::B() { cout << "cons B()\n"; }
+B::~B() { cout << "des ~B()\n"; }
+C::C() { cout << "cons C()\n"; }
+C::~C() { cout << "des ~C()\n"; }
+D::D() { cout << "cons D()\n"; }
+D::~D() { cout << "des ~D()\n"; }
+E::E() { cout << "cons E()\n"; }
+E::~E() { cout << "des ~E()\n"; }
+F::F() { cout << "cons F()\n"; }
+F::~F() { cout << "des ~F()\n"; }
+G::G() { cout << "cons G()\n"; }
+G::~G() { cout << "des ~G()\n"; }
