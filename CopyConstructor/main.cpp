c8ddf4763374b6801ddfb3694a55cd53e3ee3e81
silpavel@outlook.com <silpavel@outlook.com>
2019-05-06 18:38:02 +0300

    test ierarhy 2

--- CopyConstructor/main.cpp
+++ CopyConstructor/main.cpp
@@ -2,10 +2,14 @@
 #include "someclasses.h"
 using namespace std;
 int main() {
-	//test how much constructor and destructor executed
+	//test how much constructor and destructor executed in stack
 	{
 		G g;
 	}
+	//test how much constructor and destructor executed in heap
+	G *pg = new G;
+	delete pg;
+
 	system("pause");
 	return 0;
 }
